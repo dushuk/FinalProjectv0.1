ad656357040bf3a5e2b0f91e433173eba4ca85cf
Dushuk Oleksandr <dushuk@gmail.com>
2024-01-23 22:24:24 +0100

    Added

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,13 +10,33 @@ repositories {
 }
 
 dependencies {
-    implementation(group = "io.github.bonigarcia", name = "webdrivermanager", version = "5.3.1")
-    implementation(group = "org.seleniumhq.selenium", name = "selenium-java", version = "4.8.1")
-    implementation(group = "org.testng", name = "testng", version = "7.1.0")
+    implementation("io.github.bonigarcia:webdrivermanager:5.3.1")
+    implementation("org.seleniumhq.selenium:selenium-java:4.8.1")
+    implementation("org.testng:testng:7.1.0")
     implementation("com.github.javafaker:javafaker:1.0.2")
-    implementation(group = "ch.qos.logback", name = "logback-classic", version = "1.3.0-alpha8")
+    implementation("ch.qos.logback:logback-classic:1.3.0-alpha8")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.7.1")
+    testImplementation("io.qameta.allure:allure-junit5:2.10.0")
+    testImplementation("io.qameta.allure:allure-java-commons:2.24.0")
 }
 
 tasks.test {
     useJUnitPlatform()
+    finalizedBy("allureReport")
+}
+
+tasks.register("allureReport") {
+    dependsOn("test")
+    doLast {
+        exec {
+            commandLine = listOf(
+                    "allure",
+                    "generate", // Команда для генерации отчетов
+                    "build/allure-results", // Папка с результатами тестов
+                    "-o", "build/allure-report" // Папка для сохранения сгенерированных отчетов
+            )
+        }
+    }
 }
